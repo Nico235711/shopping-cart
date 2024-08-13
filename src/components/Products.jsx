ac2d7bb5a81de5da3c26b7c53ad3ad2042bce05e
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-12 23:00:39 -0300

    renderizando los productos

---
+++ src/components/Products.jsx
@@ -0,0 +1,31 @@
+import { formatCurrency } from "../utils/formatCurrency"
+import { AddToCartIcon } from "./Icons"
+
+export const Products = ({ products }) => {
+
+  return (
+    <main className="container mx-auto my-10">
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-5">
+        {
+          products.map(product => (
+            <li
+              key={product.id}
+              className="bg-slate-100 border-2 p-5 rounded-md shadow h-[508px]"
+            >
+              <img src={product.thumbnail} alt={product.title} />
+              <div className="mt-10">
+                <h2 className="text-xl w-60 h-[56px] font-bold">{product.title}</h2>
+                <p className="text-lg my-2 font-bold">{formatCurrency(product.price)}</p>
+              </div>
+              <div className="flex justify-end">
+                <button type="button" className="border-2 p-1 rounded-lg">
+                  <AddToCartIcon />
+                </button>
+              </div>
+            </li>
+          ))
+        }
+      </ul>
+    </main>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'outrageous-orange': {
+          '50': '#fff2ed',
+          '100': '#ffe1d4',
+          '200': '#ffc0a8',
+          '300': '#ff9471',
+          '400': '#ff5833',
+          '500': '#fe3211',
+          '600': '#ef1907',
+          '700': '#c60c08',
+          '800': '#9d0f11',
+          '900': '#7e1012',
+          '950': '#44060a',
+        },
+
+      }
+    },
   },
   plugins: [],
 }
