87314abe947d7e47a591ce0a0dbbe3638b4f639f
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-12 22:58:44 -0300

    haciendo un estado para filtrar los productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react"
+import { Products } from "./components/Products"
+import { products } from './data/products.json'
+import { Header } from "./components/Header"
 
 function App() {
 
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0
+  })
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return product.price >= filters.minPrice &&
+        (filters.category === "all" || product.category === filters.category)
+    })
+  }
+  const filteredProducts = filterProducts(products)
+
   return (
     <>
-      <h1>Shopping Cart </h1>
+      <Header products={filteredProducts} setFilters={setFilters} />
+      <Products products={filteredProducts} />
     </>
   )
 }
