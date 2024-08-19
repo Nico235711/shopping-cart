d9e9c08f04a6bef0585267eaba9365318b2cad0b
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-18 22:01:59 -0300

    creando una variable para calcular el total

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,76 +1,74 @@
-import { useId, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { CartIcon } from "./Icons"
-import { useShoppingCartContext } from "../hooks/useShoppingCartContext";
-import { formatCurrency } from "../utils/formatCurrency";
+import { useCartContext } from "../hooks/useCartContext"
+import { formatCurrency } from "../utils/formatCurrency"
 
 export const Cart = () => {
+  const [checked, setChecked] = useState(false)
+  const { cart, removeFromCart, decreaseQuantity, increaseQuantity, cleanCart } = useCartContext()
+  const isEmpty = useMemo(() => cart.length === 0, [cart])
+  const cartTotal = useMemo(() => cart.reduce((total, item) => total + (item.quantity * item.price), 0), [cart])
 
-  const cartCheckboxId = useId()
-  const [isChecked, setIsChecked] = useState(false);
-  const { cart, cleanCart } = useShoppingCartContext()
-
-  const handleCleanCart = () => {
-    setIsChecked(false)
-    cleanCart()
-  }
+  useEffect(() => {
+    setChecked(false)
+  }, [isEmpty]);
 
   return (
     <>
       <label
-        htmlFor={cartCheckboxId}
-        className="absolute top-5 right-5 bg-blue-300 p-5 rounded-full cursor-pointer hover:scale-105 transition-all z-[9999]"
+        className="absolute top-5 right-5 bg-outrageous-orange-200 rounded-full p-3 cursor-pointer z-[9999]"
+        onClick={() => setChecked(!checked)}
       >
         <CartIcon />
       </label>
-      <input
-        type="checkbox"
-        name="cart"
-        id={cartCheckboxId}
-        hidden
-        checked={isChecked}
-        onChange={() => setIsChecked(!isChecked)}
-      />
 
-      <aside className={`cart transition-all ${isChecked ? 'h-full block' : 'h-0 hidden'} fixed top-0 right-0 w-96 bg-black p-8 overflow-y-scroll`}>
-        <ul className="space-y-5">
-          {
-            cart.map(product => (
-              <li
-                key={product.id}
-                className="border-2 rounded-lg flex flex-col justify-center items-center"
-              >
-                <img
-                  src={product.thumbnail}
-                  alt={product.title}
-                  width={200}
-                />
-                <div className="space-y-3">
-                  <h2 className="text-xl w-60 font-bold text-white">{product.title}</h2>
-                  <p className="text-lg font-bold text-white">{formatCurrency(product.price)}</p>
-                  <div className="flex gap-3 text-white text-lg font-bold">
-                    <button
-                      type="button"
-                      className="bg-blue-700 py-1 px-2 rounded-lg mb-5"
-                    >+</button>
-                    <p>x{product.quantity}</p>
-                    <button
-                      type="button"
-                      className="bg-red-700 py-1 px-2 rounded-lg mb-5"
-                    >-</button>
-                  </div>
-                </div>
-              </li>
-            ))
-          }
-        </ul>
-
-        <footer>
-          <button
-            type="button"
-            className="border-2 text-white text-lg my-5 py-1 px-3 w-full rounded-lg hover:bg-blue-400 hover:opacity-70 transition-all"
-            onClick={handleCleanCart}
-          >Clean Cart</button>
-        </footer>
+      <aside className={`${checked ? "block" : "hidden"} bg-black w-96 fixed top-0 right-0 h-full overflow-y-scroll scroll-bar-aside text-white`}>
+        {
+          isEmpty ? (
+            <p className="text-center mt-20">El carrito esta vacío</p>
+          ) : (
+            <ul className="mt-20 mx-5 space-y-5">
+              {
+                cart.map(item => (
+                  <li key={item.id} className="border-2 space-y-2 p-2 flex items-center justify-between">
+                    <img
+                      src={item.thumbnail}
+                      alt={item.title}
+                      width={100}
+                      height={100}
+                    />
+                    <div>
+                      <button
+                        type="button"
+                        className="text-xl float-end"
+                        onClick={() => removeFromCart(item.id)}
+                      >❌</button>
+                      <h3 className="text-xl mb-3">{item.title}</h3>
+                      <p className="text-lg">{formatCurrency(item.price)}</p>
+                      <div className="flex justify-end items-center mt-5 gap-5">
+                        <button
+                          type="button"
+                          className="bg-blue-600 rounded-full p-1"
+                          onClick={() => increaseQuantity(item.id)}
+                        >➕</button>
+                        <p>Quantity: x{item.quantity}</p>
+                        <button
+                          type="button"
+                          className="bg-red-600 rounded-full p-1"
+                          onClick={() => decreaseQuantity(item.id)}
+                        >➖</button>
+                      </div>
+                    </div>
+                  </li>
+                ))
+              }
+              <div className="flex justify-around items-center mt-5">
+                <button type="button" className="border-2 px-2 py-1" onClick={cleanCart}>Clean Cart</button>
+                <p>Total: <span className="text-lg text-blue-400">{formatCurrency(cartTotal)}</span></p>
+              </div>
+            </ul>
+          )
+        }
       </aside>
     </>
   )
