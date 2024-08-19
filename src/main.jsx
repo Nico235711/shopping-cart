aeb7239b546acfe0e0113559d771265ac3dd470d
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-08-18 21:59:52 -0300

    consumiendo el provider del carrito

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import ShoppingCartProvider from './context/ShoppinCartProvider.jsx'
+import FilterProvider from './context/FilterProvider.jsx'
 import './index.css'
+import CartProvider from './context/CartProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ShoppingCartProvider>
-      <App />
-    </ShoppingCartProvider>
+    <CartProvider>
+      <FilterProvider>
+        <App />
+      </FilterProvider>
+    </CartProvider>
   </StrictMode>,
 )
